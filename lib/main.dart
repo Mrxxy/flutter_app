403ec45a95a9c13f001961a9b3c3caf8d8ada53b
xiao <xych45@gmail.com>
2018-08-25 11:49:23 +0800

    add bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
+///无状态的组件
+class TextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("text widget")),
+      body: new Center(
+        child: new Text(
+          "Hello World",
+          style: new TextStyle(fontSize: 18.0, color: Colors.red),
+        ),
+      ),
+      bottomNavigationBar: new BottomNavigationBar(
+        items: _buildBottom(),
+      ),
+    );
+  }
+
+  _buildBottom() {
+    final _item = new List<BottomNavigationBarItem>();
+    final _textStyle = new TextStyle(color: Colors.black);
+    _item.add(new BottomNavigationBarItem(
+        icon: new Icon(
+          Icons.favorite,
+          color: Colors.grey,
+        ),
+        title: new Text(
+          "all",
+          style: _textStyle,
+        )));
+    _item.add(new BottomNavigationBarItem(
+        icon: new Icon(
+          Icons.favorite,
+          color: Colors.red,
+        ),
+        title: new Text(
+          "collect",
+          style: _textStyle,
+        )));
+    return _item;
+  }
+}
+
 class RandomWords extends StatefulWidget {
   @override
   createState() => new RandomWordsState();
@@ -32,17 +76,53 @@ class RandomWordsState extends State<RandomWords> {
 
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  var _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Demo App'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
+        appBar: new AppBar(
+          title: new Text('Demo App'),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+          ],
+        ),
+        bottomNavigationBar: new BottomNavigationBar(
+            items: _buildBottom(),
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            }),
+        body: new IndexedStack(
+          children: <Widget>[_buildSuggestions(), new CollectApp()],
+          index: _currentIndex,
+        ));
+  }
+
+  _buildBottom() {
+    final _item = new List<BottomNavigationBarItem>();
+    final _textStyle = new TextStyle(color: Colors.black);
+    _item.add(new BottomNavigationBarItem(
+        icon: new Icon(
+          Icons.favorite,
+          color: Colors.grey,
+        ),
+        title: new Text(
+          "all",
+          style: _textStyle,
+        )));
+    _item.add(new BottomNavigationBarItem(
+        icon: new Icon(
+          Icons.favorite,
+          color: Colors.red,
+        ),
+        title: new Text(
+          "collect",
+          style: _textStyle,
+        )));
+    return _item;
   }
 
   Widget _buildSuggestions() {
@@ -97,5 +177,21 @@ class RandomWordsState extends State<RandomWords> {
           appBar: new AppBar(title: new Text('CollectList')),
           body: new ListView(children: divideTiles));
     }));
+  }
+}
+
+class CollectApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    var take = generateWordPairs().take(10);
+    final tiles = take.map((pair) {
+      return new ListTile(
+          title: new Text(pair.asPascalCase,
+              style: new TextStyle(fontSize: 18.0)));
+    });
+    final divideTiles =
+        ListTile.divideTiles(context: context, tiles: tiles).toList();
+    return new ListView(children: divideTiles);
   }
 }
